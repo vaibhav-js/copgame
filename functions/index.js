9c5df107ab1da55096d7d4456cedd4cb6a5c713c
vaibhav-js <vaibhavrajpianist@gmail.com>
2024-05-06 23:47:46 +0530

    first commit

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Weighted graph representing cities and distances
 const citiesGraph = {
   "Yapkashnagar": { distance: 60},
   "Lihaspur": { distance: 50},
@@ -23,46 +21,38 @@ const vehicles = [
   { type: "EV SUV", range: 120}
 ];
 
-// Get list of cities
 const cities = Object.keys(citiesGraph);
 
-  // Function to generate criminal city
-  function generateCriminalCity() {
-    return cities[Math.floor(Math.random() * cities.length)];
-  }
-
-
-// Simulate investigation for each cop
-function simulateInvestigation(copCity, copVehicle, criminalCity) {
-  const criminalDistance = citiesGraph[criminalCity].distance;
-
-  // Find the range of the selected vehicle
-  const vehicleRange = vehicles.find(vehicle => vehicle.type === copVehicle)?.range || 0;
-
-  // Check if the cop city is the same as the criminal city
-  if (copCity === criminalCity) {
-      // Check if the vehicle range is greater than or equal to round trip to criminal city
-      if (vehicleRange >= 2 * criminalDistance) {
-          return true; // Cop can catch the criminal
-      }
-  }
-
-  return false; // Cop cannot catch the criminal
+function generateCriminalCity() {
+  return cities[Math.floor(Math.random() * cities.length)];
 }
 
 
-// Endpoint to investigate and retrieve results
+function simulateInvestigation(copCity, copVehicle, criminalCity) {
+  const criminalDistance = citiesGraph[criminalCity].distance;
+
+  const vehicleRange = vehicles.find(vehicle => vehicle.type === copVehicle)?.range || 0;
+
+
+  if (copCity === criminalCity) {
+
+      if (vehicleRange >= 2 * criminalDistance) {
+          return true;
+      }
+  }
+
+  return false; 
+}
+
 app.post('/investigate', (req, res) => {
 
-  let criminalCity = generateCriminalCity(); // Generate criminal city when the server starts
+  let criminalCity = generateCriminalCity();
   console.log(criminalCity)
 
   const { cops } = req.body;
   
-  // Ensure that each cop object is defined and has the expected properties
   const [cop1, cop2, cop3] = cops;
   
-  // Your existing code for investigation simulation
   const result = {
     cop1: simulateInvestigation(cop1.city, cop1.vehicle, criminalCity),
     cop2: simulateInvestigation(cop2.city, cop2.vehicle, criminalCity),
@@ -76,9 +66,8 @@ app.post('/investigate', (req, res) => {
 });
 
 
-// Endpoint to signal the start of a new game session
 app.post('/new-game', (req, res) => {
-  criminalCity = generateCriminalCity(); // Generate new criminal city for the new game session
+  criminalCity = generateCriminalCity(); 
   console.log(criminalCity)
   res.set('Content-Type', 'text/plain').send('OK');
 });
